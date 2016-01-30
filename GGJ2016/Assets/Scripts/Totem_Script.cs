0efdb9e0ae8c448b45f13659e8a21d9836f0072d
Joon Ho <jhwj9617@live.com>
2016-01-29 23:36:55 -0800

    init totem script

---
+++ GGJ2016/Assets/Scripts/Totem_Script.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Totem_Script : MonoBehaviour {
+    private string attack = null;
+    private string defense = null;
+    private string support = null;
+
+    private GameObject attackSegment;
+    private GameObject defenseSegment;
+    private GameObject supportSegment;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+
+    void Update () {
+        if (Input.GetKeyUp(KeyCode.Z)){
+            buildAttack();
+        }
+        if (Input.GetKeyUp(KeyCode.X)){
+            buildDefense();
+        }
+        if (Input.GetKeyUp(KeyCode.C)){
+            buildSupport();
+        }
+    }
+
+    void buildAttack() {
+        addSegment("AttackSegment", attackSegment);
+    }
+
+    void buildDefense() {
+        addSegment("DefenseSegment", defenseSegment);
+    }
+
+    void buildSupport() {
+        addSegment("SupportSegment", supportSegment);
+    }
+
+    private void addSegment(string name, GameObject segment) {
+        segment = new GameObject(name);
+        SpriteRenderer renderer = segment.AddComponent<SpriteRenderer>();
+        renderer.sprite = Resources.Load <Sprite> ("Sprites/Square");
+        segment.transform.parent = this.transform;
+        segment.transform = this.transform;
+    }
+
+
+    void setAttack(string attack) {
+        this.attack = attack;
+    }
+
+    void setDefense(string defense) {
+        this.defense = defense;
+    }
+
+    void setSupport(string support) {
+        this.support = support;
+    }
+
+    public string getAttack() {
+        return attack;
+    }
+
+    public string getDefense() {
+        return defense;
+    }
+
+    public string getSupport() {
+        return support;
+    }
+}
