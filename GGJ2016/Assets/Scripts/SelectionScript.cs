17f6046f2dd52a93354a4d1263127caea19d8b4b
Tarek Chaya <tarekalexanderchaya@gmail.com>
2016-01-30 15:28:41 -0800

    Small changes to Selection

--- GGJ2016/Assets/Scripts/SelectionScript.cs
+++ GGJ2016/Assets/Scripts/SelectionScript.cs
@@ -14,11 +14,15 @@ public class SelectionScript : MonoBehaviour {
 	public bool p1Ready;
 	public bool p2Ready;
 	public bool allReady;
+	public bool CombatEnded = true;
 
 	private float countDown;
 	private string[] randomTotem = new string[5] {Const.FIRE, Const.WATER, Const.EARTH, Const.METAL, Const.WOOD};
 
 	public GameObject TimerLabel;
+	private UILabel _TimerLabel;
+
+	public GameObject Simulation;
 
 
 	// FIRE -> WOOD -> WATER -> EARTH -> METAL
@@ -38,6 +42,7 @@ public class SelectionScript : MonoBehaviour {
 		p2Ready = false;
 		allReady = false;
 
+		TimerLabel.SetActive (true);
 		countDown = 20;
 	}
 
@@ -53,6 +58,8 @@ public class SelectionScript : MonoBehaviour {
 	void Start () {
 		Reinitialization (); 
 
+		_TimerLabel = TimerLabel.GetComponent<UILabel>();
+
 	}
 		
 
@@ -60,169 +67,182 @@ public class SelectionScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		if (CombatEnded == true) {
 
-	
-		if (!allReady) {
+			_TimerLabel.text = "" + (Mathf.Round(countDown));
+		
+			if (!allReady) {
 
-			if (countDown <= 0) {
+				if (countDown <= 0) {
 
-				// randomize totem blocks
-				if (p1a == null){
-					p1a = chooseRandomTotem (); 
+					TimerLabel.SetActive (false);
+
+					// randomize totem blocks
+					if (p1a == null){
+						p1a = chooseRandomTotem (); 
+					}
+					if (p1d == null) {
+						p1d = chooseRandomTotem ();
+					}
+					if (p2a == null) {
+						p2a = chooseRandomTotem (); 
+					}
+					if (p2d == null) {
+						p2d = chooseRandomTotem (); 
+						
+					}
+
+					Debug.Log("P1a: " + p1a + " P2a: " + p2a);
+					Debug.Log("P1d: " + p1d + " P2d: " + p2d);
+
+					p1Ready = true;
+					p2Ready = true;
 				}
-				if (p1d == null) {
-					p1d = chooseRandomTotem ();
+
+				if (countDown > 0) {
+
+					countDown -= Time.deltaTime;
+
 				}
-				if (p2a == null) {
-					p2a = chooseRandomTotem (); 
+
+				if (!p1Ready || !p2Ready) {
+
+
+					// Player 1 Selections
+					if (!a1Ready) {
+						if (Input.GetKeyDown (KeyCode.Alpha1)) {
+							p1a = Const.FIRE;
+							a1Ready = true;
+							print("Player1Attack = FIRE");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+							p1a = Const.WOOD;
+							a1Ready = true;
+							print("Player1Attack = WOOD");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+							p1a = Const.WATER;
+							a1Ready = true;
+							print("Player1Attack = WATER");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha4)) {
+							p1a = Const.METAL;
+							a1Ready = true;
+							print("Player1Attack = METAL");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha5)) {
+							p1a = Const.EARTH;
+							a1Ready = true;
+							print("Player1Attack = EARTH");
+						}
+
+					}
+					else if(!d1Ready) {
+						if (Input.GetKeyDown (KeyCode.Alpha1)) {
+							p1d = Const.FIRE;
+							d1Ready = true;
+							p1Ready = true;
+							print("Player1Defense = FIRE, Player 1 Ready");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+							p1d = Const.WOOD;
+							d1Ready = true;
+							p1Ready = true;
+							print("Player1Defense = WOOD, Player 1 Ready");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+							p1d = Const.WATER;
+							d1Ready = true;
+							p1Ready = true;
+							print("Player1Defense = WATER, Player 1 Ready");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha4)) {
+							p1d = Const.METAL;
+							d1Ready = true;
+							p1Ready = true;
+							print("Player1Defense = METAL, Player 1 Ready");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha5)) {
+							p1d = Const.EARTH;
+							d1Ready = true;
+							p1Ready = true;
+							print("Player1Defense = EARTH, Player 1 Ready");
+						}
+
+					}
+
+
+					// Player 2 Selections
+					if (!a2Ready) {
+						if (Input.GetKeyDown (KeyCode.Alpha6)) {
+							p2a = Const.FIRE;
+							a2Ready = true;
+							print("Player2Attack = FIRE");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha7)) {
+							p2a = Const.WOOD;
+							a2Ready = true;
+							print("Player2Attack = WOOD");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha8)) {
+							p2a = Const.WATER;
+							a2Ready = true;
+							print("Player2Attack = WATER");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha9)) {
+							p2a = Const.METAL;
+							a2Ready = true;
+							print("Player2Attack = METAL");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha0)) {
+							p2a = Const.EARTH;
+							a2Ready = true;
+							print("Player2Attack = EARTH");
+						}
+
+					}
+					else if (!d2Ready) {
+						if (Input.GetKeyDown (KeyCode.Alpha6)) {
+							p2d = Const.FIRE;
+							d2Ready = true;
+							p2Ready = true;
+							print("Player2Defense = FIRE, Player 2 Ready");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha7)) {
+							p2d = Const.WOOD;
+							d2Ready = true;
+							p2Ready = true;
+							print("Player2Defense = WOOD, Player 2 Ready");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha8)) {
+							p2d = Const.WATER;
+							d2Ready = true;
+							p2Ready = true;
+							print("Player2Defense = WATER, Player 2 Ready");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha9)) {
+							p2d = Const.METAL;
+							d2Ready = true;
+							p2Ready = true;
+							print("Player2Defense = METAL, Player 2 Ready");
+						}
+						else if (Input.GetKeyDown (KeyCode.Alpha0)) {
+							p2d = Const.EARTH;
+							d2Ready = true;
+							p2Ready = true;
+							print("Player2Defense = EARTH, Player 2 Ready");
+						}
+
+					}
+				} 
+				else if (p1Ready && p2Ready) {
+					allReady = true;
+					TimerLabel.SetActive (false);
+					print("Both players ready");
 				}
-				if (p2d == null) {
-					p2d = chooseRandomTotem (); 
-					
-				}
-				p1Ready = true;
-				p2Ready = true;
 			}
-
-			if (countDown > 0) {
-
-				countDown -= Time.deltaTime;
-
-			}
-
-			if (!p1Ready || !p2Ready) {
-
-
-				// Player 1 Selections
-				if (!a1Ready) {
-					if (Input.GetKeyDown (KeyCode.Alpha1)) {
-						p1a = Const.FIRE;
-						a1Ready = true;
-						print("Player1Attack = FIRE");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha2)) {
-						p1a = Const.WOOD;
-						a1Ready = true;
-						print("Player1Attack = WOOD");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha3)) {
-						p1a = Const.WATER;
-						a1Ready = true;
-						print("Player1Attack = WATER");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha4)) {
-						p1a = Const.METAL;
-						a1Ready = true;
-						print("Player1Attack = METAL");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha5)) {
-						p1a = Const.EARTH;
-						a1Ready = true;
-						print("Player1Attack = EARTH");
-					}
-
-				}
-				else if(!d1Ready) {
-					if (Input.GetKeyDown (KeyCode.Alpha1)) {
-						p1d = Const.FIRE;
-						d1Ready = true;
-						p1Ready = true;
-						print("Player1Defense = FIRE, Player 1 Ready");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha2)) {
-						p1d = Const.WOOD;
-						d1Ready = true;
-						p1Ready = true;
-						print("Player1Defense = WOOD, Player 1 Ready");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha3)) {
-						p1d = Const.WATER;
-						d1Ready = true;
-						p1Ready = true;
-						print("Player1Defense = WATER, Player 1 Ready");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha4)) {
-						p1d = Const.METAL;
-						d1Ready = true;
-						p1Ready = true;
-						print("Player1Defense = METAL, Player 1 Ready");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha5)) {
-						p1d = Const.EARTH;
-						d1Ready = true;
-						p1Ready = true;
-						print("Player1Defense = EARTH, Player 1 Ready");
-					}
-
-				}
-
-
-				// Player 2 Selections
-				if (!a2Ready) {
-					if (Input.GetKeyDown (KeyCode.Alpha6)) {
-						p2a = Const.FIRE;
-						a2Ready = true;
-						print("Player2Attack = FIRE");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha7)) {
-						p2a = Const.WOOD;
-						a2Ready = true;
-						print("Player2Attack = WOOD");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha8)) {
-						p2a = Const.WATER;
-						a2Ready = true;
-						print("Player2Attack = WATER");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha9)) {
-						p2a = Const.METAL;
-						a2Ready = true;
-						print("Player2Attack = METAL");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha0)) {
-						p2a = Const.EARTH;
-						a2Ready = true;
-						print("Player2Attack = EARTH");
-					}
-
-				}
-				else if (!d2Ready) {
-					if (Input.GetKeyDown (KeyCode.Alpha6)) {
-						p2d = Const.FIRE;
-						d2Ready = true;
-						p2Ready = true;
-						print("Player2Defense = FIRE, Player 2 Ready");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha7)) {
-						p2d = Const.WOOD;
-						d2Ready = true;
-						p2Ready = true;
-						print("Player2Defense = WOOD, Player 2 Ready");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha8)) {
-						p2d = Const.WATER;
-						d2Ready = true;
-						p2Ready = true;
-						print("Player2Defense = WATER, Player 2 Ready");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha9)) {
-						p2d = Const.METAL;
-						d2Ready = true;
-						p2Ready = true;
-						print("Player2Defense = METAL, Player 2 Ready");
-					}
-					else if (Input.GetKeyDown (KeyCode.Alpha0)) {
-						p2d = Const.EARTH;
-						d2Ready = true;
-						p2Ready = true;
-						print("Player2Defense = EARTH, Player 2 Ready");
-					}
-
-				}
-			} 
-			else if (p1Ready && p2Ready) {
-				allReady = true;
-				print("Both players ready");
+			else {
+				// send attack and defense to simulation
 			}
 		}
 	}
